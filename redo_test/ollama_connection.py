d0d58d51ac476e2eacdf3921748b8beffd50987f
jinxu-ai <taojinxu7@gmail.com>
2024-12-05 18:56:04 +0800

    Update ollama_connection.py

--- redo_test/ollama_connection.py
+++ redo_test/ollama_connection.py
@@ -1,11 +1,15 @@
 from ollama import chat
 from ollama import ChatResponse
+import ollama
 
-response: ChatResponse = chat(
-    model='llama3.2', 
-    messages=[{
-        'role': 'user',
-        'content': 'Why is the sky blue?'}])
-print(response['message']['content'])
-# or access fields directly from the response object
-print(response.message.content)
+# response: ChatResponse = chat(
+#     model='llama3.2', 
+#     messages=[{
+#         'role': 'user',
+#         'content': 'Why is the sky blue?'}])
+# print(response['message']['content'])
+# # or access fields directly from the response object
+# print(response.message.content)
+
+chat_test = ollama.chat(model='llama3.2', messages=[{'role': 'user', 'content': 'Why is the sky blue?'}])
+print(type(chat_test))
